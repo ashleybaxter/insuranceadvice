0fe2eef8051c24dab1a1fc81fb0c9031e2e80959
ashleybaxter <ashleybaxter@me.com>
2013-09-02 15:33:27 +0100

    Search changed for PG
    PG is case sensitive, unlike SQlite.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,7 +3,7 @@ class Entry < ActiveRecord::Base
   
   def self.search(search)
     if search
-      find(:all, :conditions => ['description or title LIKE ?', "%#{search}%"])
+      find(:all, :conditions => ['description or title LIKE ?', "%#{search.upcase}%"])
     else
       find(:all)
     end
