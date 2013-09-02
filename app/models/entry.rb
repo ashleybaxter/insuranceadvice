a97f5ca238d78243043f71d7a5d5c449cb126f42
ashleybaxter <ashleybaxter@me.com>
2013-09-02 15:44:52 +0100

    Search changed for PG (upcase)
    Previous TWO commits didn't work. Gosh, Ashley.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,7 +3,7 @@ class Entry < ActiveRecord::Base
   
   def self.search(search)
     if search
-      find(:all, :conditions => ['description or title ILIKE ?', "%#{search}%"])
+      find(:all, :conditions => ['UPPER(description) or UPPER(title) LIKE ?', "%#{search.upcase}%"])
     else
       find(:all)
     end
