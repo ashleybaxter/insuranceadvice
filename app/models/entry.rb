8845164ace7c6c7981784b81f8921c0a980608bf
ashleybaxter <ashleybaxter@me.com>
2013-09-03 11:00:43 +0100

    URL includes title

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,10 @@
 class Entry < ActiveRecord::Base
   attr_accessible :description, :title
   
+  def to_param
+    "#{id} #{title}".parameterize
+  end
+  
   def self.search(search)
     if search.present?
       where("description ilike :q or title ilike :q", q: "%#{search}%")
